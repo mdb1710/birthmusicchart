49bf0bd3a845bd2699869ad81f621bcc8ff29948
mdb1710 <mdb1710@gmail.com>
2020-01-30 17:31:40 -0500

    injection of service into Results component

--- src/app/billboard.service.ts
+++ src/app/billboard.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from "@angular/core";
 
+import { HttpClient } from '@angular/common/http'
+
 @Injectable({
   providedIn: "root"
 })
 export class BillboardService {
-  constructor() {}
+
+  uri = 'http://localhost:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  getBirthChart(date) {
+    return this.http.get(`${this.uri}/charts`, date)
+  };
+
+
 }
