a25fc673f3ca5f0de729c65a1dd04f6c585f5de9
mdb1710 <mdb1710@gmail.com>
2020-01-30 16:08:36 -0500

    added to billboardservice

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 
-import { AngularBillboardService } from "angular-billboard";
-
 @Component({
   selector: "app-results",
   templateUrl: "./results.component.html",
@@ -11,24 +9,7 @@ export class ResultsComponent implements OnInit {
   charts: any = [];
   chartsOptions: any = [];
 
-  constructor(private angularBillboardService: AngularBillboardService) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.chartsOptions = [
-      {
-        data: {
-          type: "bar",
-          columns: [
-            ["data1", 30, 200, 100, 170, 150, 250],
-            ["data2", 130, 100, 140, 35, 110, 50],
-            ["data3", 10, 80, 240, 35, 11, 70]
-          ]
-        },
-        title: {
-          text: "Bar"
-        }
-      }
-    ];
-    this.charts = this.angularBillboardService.generate(...this.chartsOptions);
-  }
+  ngOnInit() {}
 }
